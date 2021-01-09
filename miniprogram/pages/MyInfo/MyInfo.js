e65f2b01d0fb391f448ac4fe9e4b0a38e3e20aae
LiChao <lc2510834510@gamil.com>
2021-01-09 17:24:05 +0800

    登录

--- miniprogram/pages/MyInfo/MyInfo.js
+++ miniprogram/pages/MyInfo/MyInfo.js
@@ -62,5 +62,50 @@ Page({
    */
   onShareAppMessage: function () {
 
+  },
+  Login:function(e){
+    wx.login({
+      success: (result) => {
+       console.log(result.code)
+        wx.request({
+          url: 'https://api.weixin.qq.com/sns/jscode2session',
+          data:{
+            appid:'wx7943ce020c0045a0',
+            secret:'0b3a05d236200c2aef660ca8837808d7',
+            js_code:result.code,
+            grant_type: 'authorization_code'
+          },
+          success (res){
+            console.log(res.data)
+          }
+        })
+      },
+      fail: (res) => {
+        console.log("code",res.code)
+      },
+    })
+  },
+  getSessionKey:function(code,appid,appsecret){
+    var opt = {
+      method: 'GET',
+      url: 'https://api.weixin.qq.com/sns/jscode2session',
+      params: {
+          appid: appid,
+          secret: appsecret,
+          js_code: code,
+          grant_type: 'authorization_code'
+      }
+    };
+    return http(opt).then(function (response) {
+       var data = response.data;
+        if (!data.openid || !data.session_key || data.errcode) {
+          return {
+              result: -2,
+              errmsg: data.errmsg || '返回数据字段不完整'
+          }
+        } else {
+           return data
+       }
+    });
   }
 })
